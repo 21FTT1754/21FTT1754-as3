fa6de3a119f8f93567644136f006897e13d4a368
Adam <21FTT1754@student.pb.edu.bn>
2023-04-14 20:29:27 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class=" bg-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-center">
+  <div class="container max-w-lg bg-white rounded dark:bg-gray-800 shadow-lg transform duration-200 easy-in-out m-12">
+    <div class="h-2/4 sm:h-64 overflow-hidden">
+      <img class="w-full rounded-t"
+      img src="src/assets/adam.jpg"
+        alt="darwish" />
+    </div>
+    <div class="flex justify-start px-5 -mt-12 mb-5">
+      <span clspanss="block relative h-32 w-32">
+        <img alt="Adam"
+          img src="src/assets/adamdr.jpg"
+          class="mx-auto object-cover rounded-full h-24 w-24 bg-white p-1" />
+      </span>
+    </div>
+    <div class="">
+      <div class="px-7 mb-8">
+        <h2 class="text-3xl font-bold text-green-800 dark:text-gray-300">Adam Andy</h2>
+        <p class="text-gray-400 mt-2 dark:text-gray-400">Student</p>
+        <p class="mt-2 text-gray-600 dark:text-gray-300">Name: Awangku Muhammad Adam Darwish Bin Pengiran Andy Suhardi. <br>Student ID: 21FTT1754 <br> Class: DWDT07 <br>Student of Politeknik Brunei.</p>
+        <div
+          class="justify-center px-4 py-2 cursor-pointer bg-green-900 max-w-min mx-auto mt-8 rounded-lg text-gray-300 hover:bg-green-800 hover:text-gray-100 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-gray-200">
+          21FTT1754@student.pb.edu.bn
+        </div>
+        <div class="flex flex-wrap justify-center gap-2 sm:gap-4 mt-8">
+          
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</div>
+
 </template>
 
 <style scoped>
